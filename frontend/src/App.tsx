e4302c7805a040c4ba36a3f6cc499c0c52ae98a3
saumyasingh003 <saumyasingh98982@gmail.com>
2025-01-31 00:58:49 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+
 import {BrowserRouter as Router, Routes , Route} from 'react-router-dom'
 import './App.css'
 import FileUploadPage from './pages/FileUploadPage.tsx'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="bg-gray-900 min-h-screen">
